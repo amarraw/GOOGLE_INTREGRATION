16b721e0d1cca0f0c5c2fe87940c1650d67ccd53
amarraw <amar.raw011@gmail.com>
2025-03-01 10:15:34 +0545

    google drive fixed

--- core/asgi.py
+++ core/asgi.py
@@ -1,19 +1,16 @@
-# your_project_name/asgi.py
+"""
+ASGI config for core project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
 
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from your_app_name import consumers
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter([
-            # URL routing for WebSockets
-            path("ws/chat/<str:room_name>/", consumers.ChatConsumer.as_asgi()),
-        ])
-    ),
-})
+application = get_asgi_application()
